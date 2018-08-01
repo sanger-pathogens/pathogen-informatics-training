ea2e222c2e8d54565c5b71e1dcf07cb04a2aad6d
vaofford <vo1@sanger.ac.uk>
2018-08-01 16:11:05 +0100

    update getting sample name with substring to replacing

--- Notebooks/RNA-Seq/data/map_SBP_samples.sh
+++ Notebooks/RNA-Seq/data/map_SBP_samples.sh
@@ -3,7 +3,7 @@
 for r1 in SBP*_1.fastq
 do
   echo $r1
-  sample=${r1:0:${#r1}-8}
+  sample=${r1/_1.fastq/}
   echo "Processing sample: "$sample
   hisat2 --max-intronlen 10000 -x PccAS_v3_hisat2.idx -1 $sample'_1.fastq' -2 $sample'_2.fastq' \
   | samtools view -b - \
